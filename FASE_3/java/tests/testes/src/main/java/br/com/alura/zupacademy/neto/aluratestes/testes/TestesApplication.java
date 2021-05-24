f7881e3faf1518e1386cee62019de933c3e24a1c
[ Neto ] Antonio Miranda <netomantonio@gmail.com>
2021-05-24 12:10:44 -0300

    add project to fase 3 testes

---
+++ FASE_3/java/tests/testes/src/main/java/br/com/alura/zupacademy/neto/aluratestes/testes/TestesApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.zupacademy.neto.aluratestes.testes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestesApplication.class, args);
+	}
+
+}
---
+++ FASE_3/java/tests/testes/src/test/java/br/com/alura/zupacademy/neto/aluratestes/testes/TestesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.zupacademy.neto.aluratestes.testes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
